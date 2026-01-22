e04a624c14cdccaf5e180d4defd1b2b9739b6259
dheim123 <dheim@midland.k12.ia.us>
2026-01-22 12:18:29 -0600

    Add CalculateAward method in Awarder class
    Implement CalculateAward method to determine award based on points.

--- Awarder.cs
+++ Awarder.cs
@@ -1,5 +1,19 @@
 ﻿namespace knightmoves;
 public class Awarder
 {
-    // Add your code here
+    public string CalculateAward(points){
+        if (point > 99){
+            return "First Place"; 
+        }
+    } else {
+        return "Participation Award";
+    }
+
+    if (points > 49 && points < 100){
+        return "Second Place";
+    } else {
+       return "Participation Award";
+    }
+    
 }
+
